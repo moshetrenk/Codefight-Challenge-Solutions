fb409cc56ac9b6ca9f25b9bb37e6285b5dd496af
Moshe Trenk <moshe.trenk@cix.csi.cuny.edu>
2018-08-05 15:38:49 -0400

    Update and rename Plus Minus to PlusMinus.java

--- PlusMinus.java
+++ PlusMinus.java
@@ -4,26 +4,28 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class PlusMinus{
 
-    public static void main(String[] args) {
+    public static String plusMinus(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int arr[] = new int[n];
-        for(int arr_i=0; arr_i < n; arr_i++){
+        for (int arr_i = 0; arr_i < n; arr_i++) {
             arr[arr_i] = in.nextInt();
         }
-        
+
         int pos = 0, negs = 0, zeroes = 0, total = arr.length;
-        
-        for(int a : arr)
-            if(a > 0)
+
+        for (int a : arr) {
+            if (a > 0) {
                 pos++;
-            else if(a == 0)
+            } else if (a == 0) {
                 zeroes++;
-            else
+            } else {
                 negs++;
-        
-        System.out.println((double)pos/total + "\n" + (double)negs/total + "\n" + (double)zeroes/total);
+            }
+        }
+
+        return ((double) pos / total + "\n" + (double) negs / total + "\n" + (double) zeroes / total);
     }
 }
