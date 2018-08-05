5189e542069b4f1bab4ac186bbe4051297bbf484
Moshe Trenk <moshe.trenk@cix.csi.cuny.edu>
2018-08-05 15:41:01 -0400

    Update and rename String Compare to StringComparer.java

--- StringComparer.java
+++ StringComparer.java
@@ -1,24 +1,23 @@
-import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class StringComparer {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String y = in.next();
         int x = in.nextInt();
-       
+
 
         ArrayList<String> subs = new ArrayList<String>();
         StringBuilder d = new StringBuilder("");
 
-        for(int i = 0; i < x; i++){
+        for (int i = 0; i < x; i++) {
             d.append(y.charAt(i));
         }
 
         subs.add(new String(d));
 
-        for( ; x < y.length(); x++){
+        for (; x < y.length(); x++) {
             d.delete(0, 1);
             d.append(y.charAt(x));
             subs.add(new String(d));
@@ -28,7 +27,7 @@ public class Solution {
 
         System.out.println(subs.get(0));
         System.out.println(subs.get(subs.size() - 1));
-        in.close();       
-        
+        in.close();
+
     }
 }
