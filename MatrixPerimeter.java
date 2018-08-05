9cc3298c6ffae1bab15bdb1d0da851f705429fc0
Moshe Trenk <moshe.trenk@cix.csi.cuny.edu>
2018-08-05 15:33:10 -0400

    Update MatrixPerimeter.java

--- MatrixPerimeter.java
+++ MatrixPerimeter.java
@@ -2,44 +2,43 @@ import java.util.*;
 
 //MatrixPerimeter... the long way
 public class MatrixPerimeter {
-    int matrixPerimeter(boolean[][] matrix) {
-        if(matrix.length == 0)
+    int MatrixPerimeter(boolean[][] matrix) {
+        if (matrix.length == 0)
             return 0;
 
         int num = 0;
-        for(boolean[] a : matrix){
-            for(boolean b: a)
-                if(b)
+        for (boolean[] a : matrix) {
+            for (boolean b : a)
+                if (b)
                     num += 4;
         }
         int i = 0, j = 0;
 
-        if(matrix.length == 1){
-            for(int a = 1; a < matrix[0].length - 1; a++){
-                if(matrix[0][a + 1])
+        if (matrix.length == 1) {
+            for (int a = 1; a < matrix[0].length - 1; a++) {
+                if (matrix[0][a + 1])
                     num--;
-                if(matrix[0][a - 1])
+                if (matrix[0][a - 1])
                     num--;
             }
             return num;
-        }
-        else{
-            for(boolean[] a : matrix){
+        } else {
+            for (boolean[] a : matrix) {
                 j = 0;
-                for(boolean b: a){
+                for (boolean b : a) {
                     System.out.println("i = " + i);
                     System.out.println("j = " + j);
-                    if(i > 0 && matrix[i][j] && matrix[i - 1][j]){
+                    if (i > 0 && matrix[i][j] && matrix[i - 1][j]) {
                         num--;
                     }
-                    if((i < (matrix.length - 1)) && matrix[i][j] && matrix[i + 1][j]){
+                    if ((i < (matrix.length - 1)) && matrix[i][j] && matrix[i + 1][j]) {
                         num--;
                     }
-                    if((j < (matrix[0].length - 1)))
-                        if(matrix[i][j] && matrix[i][j + 1]){
+                    if ((j < (matrix[0].length - 1)))
+                        if (matrix[i][j] && matrix[i][j + 1]) {
                             num--;
                         }
-                    if(j > 0 && matrix[i][j] && matrix[i][j - 1]){
+                    if (j > 0 && matrix[i][j] && matrix[i][j - 1]) {
                         num--;
                     }
 
