2226c0bc8a08de17f052f68b372bbad70cf751bf
Moshe Trenk <moshe.trenk@cix.csi.cuny.edu>
2018-08-05 15:46:44 -0400

    Update and rename Time Conversions to TimeConversion.java

--- TimeConversion.java
+++ TimeConversion.java
@@ -1,29 +1,25 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
-public class Solution {
+public class TimeConversion {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String time = in.next();
-       if(time.substring(8).equals("AM") && time.substring(0, 2).equals("12")){
-        	time = "00" + time.substring(2);
+        if(time.substring(8).equals("AM") && time.substring(0, 2).equals("12")){
+            time = "00" + time.substring(2);
         }
-       
+
         int intVal = Integer.valueOf(time.replace(":", "").substring(0, 6));
         intVal += (time.substring(8).equals("PM") && !time.substring(0, 2).equals("12") ? 120000 : 0);
-        
+
         time = String.valueOf(intVal);
-        
-        while(time.length() <= 5){
-        	time = "0" + time;
+
+        while(time.length() <= 5) {
+            time = "0" + time;
         }
-        
+
         System.out.println(time.substring(0, 2) + ":" + time.substring(2, 4) + ":" + time.substring(4));
-        
-        
+
+
     }
 }
