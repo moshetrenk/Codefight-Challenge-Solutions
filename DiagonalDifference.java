59b430754f2e9515049c0585971157a748fa01e6
Moshe Trenk <moshe.trenk@cix.csi.cuny.edu>
2018-08-05 15:27:55 -0400

    Update and rename Diagonal Difference to DiagonalDifference.java

--- DiagonalDifference.java
+++ DiagonalDifference.java
@@ -1,28 +1,23 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
-public class Solution {
+public class DiagonalDifference {
 
-    public static void main(String[] args) {
+    public static int getDiagonalDifference() {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int a[][] = new int[n][n];
-        for(int a_i=0; a_i < n; a_i++){
-            for(int a_j=0; a_j < n; a_j++){
+        for(int a_i = 0; a_i < n; a_i++){
+            for(int a_j = 0; a_j < n; a_j++){
                 a[a_i][a_j] = in.nextInt();
             }
         }
-        
+
         int sum1 = 0, sum2 = 0;
         for(int i = 0; i < a.length; i++){
             sum1 += a[i][i];
             sum2 += a[i][a.length - i - 1];
         }
-        
-        System.out.println(Math.abs(sum1 - sum2));
-        
+
+        return Math.abs(sum1 - sum2);
     }
 }
