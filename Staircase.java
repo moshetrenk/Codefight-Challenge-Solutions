6bec68f677a2706de042df96213870505ef513d9
Moshe Trenk <moshe.trenk@cix.csi.cuny.edu>
2018-08-05 15:40:30 -0400

    Update and rename Staircase to Staircase.java

--- Staircase.java
+++ Staircase.java
@@ -1,27 +1,23 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
-public class Solution {
+public class Staircase {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int x = 1;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n - x; j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - x; j++) {
                 System.out.print(' ');
             }
-            
-            for(int j = 0; j < x; j++){
+
+            for (int j = 0; j < x; j++) {
                 System.out.print('#');
             }
-            
+
             x++;
             System.out.println();
-            
-        }        
+
+        }
     }
 }
