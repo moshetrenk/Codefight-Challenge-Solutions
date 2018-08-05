522ffab3d8dbc6f710804d77625ca429df07aea6
Moshe Trenk <moshe.trenk@cix.csi.cuny.edu>
2018-08-05 15:49:11 -0400

    Update and rename lonelyInteger.java to LonelyInteger.java

---
+++ LonelyInteger.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class LonelyInteger {
+
+    private static int lonelyinteger(int[] a) {
+        Arrays.sort(a);
+
+        for (int i = 0; i < a.length; i += 2) {
+            try {
+                if (a[i] != a[i + 1]) {
+                    return a[i];
+                }
+            } catch (Exception e) {
+                return a[i];
+            }
+        }
+        return a[a.length - 1];
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for (int a_i = 0; a_i < n; a_i++) {
+            a[a_i] = in.nextInt();
+        }
+        int result = lonelyinteger(a);
+        System.out.println(result);
+    }
+}
